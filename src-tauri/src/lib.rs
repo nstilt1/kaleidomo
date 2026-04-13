bec0c12e35bcda43f8008e497c674b4af850aa66
Noah Stiltner <nstilt1@lsu.edu>
2026-04-13 13:55:57 -0500

    0.9.11

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,4 @@
-const VERSION: &str = "0.9.10";
+const VERSION: &str = "0.9.11";
 const PRODUCT_NAME: &str = "Kaleidomo";
 const DOWNLOADS_URL: &str = "https://alteredbrainchemistry.com/dashboard/downloads";
 const STORE_PAGE_URL: &str = "https://alteredbrainchemistry.com/shop/kaleidomo-kaleidoscope-generator/";
